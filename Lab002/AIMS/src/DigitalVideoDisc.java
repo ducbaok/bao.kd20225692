d446246a495f104e4f76c76a586b62447c96e261
ducbaok <ducbaodaidong@gmail.com>
2024-11-23 11:44:00 +0700

    topic/class-members

--- Lab002/AIMS/src/DigitalVideoDisc.java
+++ Lab002/AIMS/src/DigitalVideoDisc.java
@@ -1,12 +1,17 @@
 
 public class DigitalVideoDisc {
+    private int id;
     private String title;
     private String category;
     private String director;
     private int length;
     private float cost;
+    private static int nbDigitalVideoDiscs = 0; 
     public String getTitle() {
         return title;
+    }
+    public int getId() {
+        return id;
     }
     public void setTitle(String title) {
         this.title = title;
@@ -37,16 +42,19 @@ public class DigitalVideoDisc {
     }
     public DigitalVideoDisc(String title) {
         super();
+        this.id = nbDigitalVideoDiscs++;
         this.title = title;
     }
     public DigitalVideoDisc(String title,String category,float cost){
         super();
+        this.id = nbDigitalVideoDiscs++;
         this.title=title;
         this.category=category;
         this.cost=cost;
     }
     public DigitalVideoDisc(String title,String director,float cost,String category){
         super();
+        this.id = nbDigitalVideoDiscs++;
         this.title=title;
         this.director=director;
         this.cost=cost;
@@ -54,10 +62,14 @@ public class DigitalVideoDisc {
     }
     public DigitalVideoDisc(String title,String category,String director,int length,float cost){
         super();
+        this.id = nbDigitalVideoDiscs++;
         this.title=title;
         this.director=director;
         this.cost=cost;
         this.length=length;
         this.category=category;
     }
+
+    
+
 }
