2b903dc102b942c33e15c365d8c086c2845dec1d
ducbaok <ducbaodaidong@gmail.com>
2024-11-23 11:23:04 +0700

    parameterv2

--- Lab002/AIMS/src/TestPassingParameter.java
+++ Lab002/AIMS/src/TestPassingParameter.java
@@ -7,6 +7,14 @@ public class TestPassingParameter {
         System.out.println("jungle dvd title: " + jungleDVD.getTitle());
         System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
 
+        
+
+        correctlyswap(jungleDVD, cinderellaDVD);
+        System.out.println("How it should be done");
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
+
+
         changeTitle(jungleDVD, cinderellaDVD.getTitle());
         System.out.println("jungle dvd title: " + jungleDVD.getTitle());
     }
@@ -22,6 +30,23 @@ public class TestPassingParameter {
         String oldTitle = dvd.getTitle();
         dvd.setTitle(title);
         dvd = new DigitalVideoDisc(oldTitle);
+    }
+
+    public static void correctlyswap(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2)
+    {
+        DigitalVideoDisc temp = new DigitalVideoDisc(dvd1.getTitle(), dvd1.getCategory(), dvd1.getDirector(), dvd1.getLength(), dvd1.getCost());
+
+        dvd1.setTitle(dvd2.getTitle());
+        dvd1.setCategory(dvd2.getCategory());
+        dvd1.setDirector(dvd2.getDirector());
+        dvd1.setLength(dvd2.getLength());
+        dvd1.setCost(dvd2.getCost());
+
+        dvd2.setTitle(temp.getTitle());
+        dvd2.setCategory(temp.getCategory());
+        dvd2.setDirector(temp.getDirector());
+        dvd2.setLength(temp.getLength());
+        dvd2.setCost(temp.getCost());
     }
 }
 
