dd0c71088aaeb0300516293abfaaee874de0712c
ducbaok <ducbaodaidong@gmail.com>
2024-11-25 16:50:45 +0700

    topic/passing-parameter

--- AIMS/src/DigitalVideoDisc.java
+++ AIMS/src/DigitalVideoDisc.java
@@ -5,6 +5,7 @@ public class DigitalVideoDisc {
     private String director;
     private int length;
     private float cost;
+
     public String getTitle() {
         return title;
     }
@@ -14,8 +15,23 @@ public class DigitalVideoDisc {
     public String getDirector() {
         return director;
     }
+    public void setTitle(String title) {
+        this.title = title;
+    }
     public int getLength() {
         return length;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public void setDirector(String director) {
+        this.director = director;
+    }
+    public void setLength(int length) {
+        this.length = length;
+    }
+    public void setCost(float cost) {
+        this.cost = cost;
     }
     public float getCost() {
         return cost;
