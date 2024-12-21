864cfea40e8974f1407c2ba82ad7f25047ce9824
ducbaok <ducbaodaidong@gmail.com>
2024-12-21 19:57:51 +0700

    virtual exit

---
+++ GUIProject/src/hust/soict/dsai/javafx/PaintController.java
@@ -0,0 +1,40 @@
+package hust.soict.dsai.javafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class PaintController{
+    private boolean UsingPen;
+    @FXML
+    private VBox drawingAreaPane;
+
+    @FXML
+    void clearButtonPressed(ActionEvent event) {
+        drawingAreaPane.getChildren().clear();
+    }
+
+    @FXML
+    void drawingAreaMouseDragged(MouseEvent event) {
+        Color color = UsingPen ? Color.BLACK : Color.WHITE;
+        Circle newCircle = new Circle(event.getX(), event.getY(), 5, color);
+        drawingAreaPane.getChildren().add(newCircle);
+    }
+
+    @FXML
+    void onPressMouse(ActionEvent e)
+    {
+        UsingPen = true;
+    }
+
+    @FXML
+    void onEraserPress(ActionEvent e)
+    {
+        UsingPen = false;
+    }
+
+}
+
